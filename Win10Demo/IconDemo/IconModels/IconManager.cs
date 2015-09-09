d1ed93c29e6664d451d268bf5ee042f3aa5e89d1
TamHuynh <tamhuynh.msp@outlook.com>
2015-09-09 21:02:26 +0700

    Icon Demo

---
+++ Win10Demo/IconDemo/IconModels/IconManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IconDemo.IconModels
+{
+    public class IconManager : INotifyPropertyChanged
+    {
+        private ObservableCollection<IconInfo> _icons;
+        public ObservableCollection<IconInfo> Icons
+        {
+            get
+            {
+                return _icons;
+            }
+
+            set
+            {
+                _icons = value;
+                NotifyPropertyChanged("Icons");
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+
+    }
+}
---
+++ Win10Demo/IconDemo/ViewModels/ViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace IconDemo.ViewModels
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+    }
+}
